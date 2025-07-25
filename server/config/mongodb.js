2ca35554b1321143aab82f8d001c1c538812de10
Lelisa Tesfaye <kekikoki4@gmail.com>
2025-07-25 15:16:11 +0300

    commit

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,9 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => console.log('database connected'));
 
-const connectDB = async () =>{
-    mongoose.connection.on('connected', ()=>console.log('database connected'));
+    await mongoose.connect(process.env.MONGO_URI);
+};
 
-    await mongoose.connect(`${process.env.MONGODB_URL} /user-auth`)
-}
-
-export default connectDB
+export default connectDB;
